46c60768272fc7fe2f56a2eaf26b5d8c61107b9b
A-Aron <vale.aaron@gmail.com>
2017-11-02 12:16:20 -0400

    Add Working Create Quiz Form
    Update createQuiz action to create a new record for quizzes with title
    and category fields.

--- app/components/create-quiz-form.js
+++ app/components/create-quiz-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newQuiz: {
+    title: null,
+    category: null
+  },
+  actions: {
+    createQuiz () {
+      this.sendAction('createQuiz', this.get('newQuiz'));
+      this.set('newQuiz', null);
+    }
+  }
 });
