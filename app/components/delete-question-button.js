30c775b29d767324a0e216ccc24b7638779852ff
A-Aron <vale.aaron@gmail.com>
2017-11-03 11:26:05 -0400

    Fix Issue with Delete Question
    Fixes issue where clicking a link to an individual question would not
    load the question model in Ember.

--- app/components/delete-question-button.js
+++ app/components/delete-question-button.js
@@ -7,6 +7,8 @@ export default Ember.Component.extend({
   actions: {
     deleteQuestion () {
       const user_id = this.get('question.quiz.user_id');
+      console.log(user_id);
+      console.log(this.get('id'));
       if (user_id === this.get('id')) {
         this.sendAction('deleteQuestion', this.get('question'));
       } else {
