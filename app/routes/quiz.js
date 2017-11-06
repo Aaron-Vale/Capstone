46950d02ab03d1e276bcd0861bf0cd30399db7cd
A-Aron <vale.aaron@gmail.com>
2017-11-06 09:21:29 -0500

    Add User Feedback Actions
    Add success/failure messages for creating
    questions and updating quiz title/category.

--- app/routes/quiz.js
+++ app/routes/quiz.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
   user_id: Ember.computed.alias('auth.credentials.id'),
   model (params) {
     return Ember.RSVP.hash({
@@ -13,19 +14,31 @@ export default Ember.Route.extend({
     createQuestion (inputs) {
       const id = inputs.quiz_id;
       let newQuestion = this.get('store').createRecord('question', inputs);
-      newQuestion.save();
-      this.model({quiz_id: id});
-      this.transitionTo('quiz', id);
+      newQuestion.save()
+      .then(() => this.get('flashMessages').success('Question created!'))
+      .then(() => this.transitionTo('quizzes'))
+      .then(() => this.transitionTo('quiz', id))
+      .catch(() =>
+        this.get('flashMessages')
+        .danger('Unable to create question. Please fill out all form fields.')
+      )
     },
     editQuiz (newQuiz, quiz) {
       quiz.set('title', newQuiz.title);
       quiz.set('category', newQuiz.category);
-      quiz.save();
-      this.modelFor('quizzes').reload();
+      quiz.save()
+        .then(() => this.get('flashMessages').success('Quiz Updated!'))
+        // .then(() => this.modelFor('quizzes').reload())
+        .catch(() => this.get('flashMessages')
+          .danger('Unable to create question. Please fill out all form fields.')
+      )
     },
     updateScore (user, score) {
       user.set('score', score);
       user.save();
+    },
+    refreshRoute () {
+      this.refresh();
     }
   }
 });
