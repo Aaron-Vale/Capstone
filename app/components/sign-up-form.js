6932d73a211a959b98e1e0bb8583c532b7ce302e
A-Aron <vale.aaron@gmail.com>
2017-11-08 10:30:44 -0500

    Fix Typo in Sign-up form

--- app/components/sign-up-form.js
+++ app/components/sign-up-form.js
@@ -3,11 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'form',
   classNames: ['form-horizontal'],
-
   credentials: {},
   actions: {
     submit () {
-      this.sendAction('submit', this.get('credentials'))
+      this.sendAction('submit', this.get('credentials'));
     },
     reset () {
       this.set('credentials', {});
